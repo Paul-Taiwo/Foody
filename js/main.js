da32bcafce953ceee8d35cffc3a65f6865d8b44c
Paul Taiwo <pauladedokun2000@gmail.com>
2018-06-29 09:40:57 +0100

    Modify the function to toggle menu for mobile

--- js/main.js
+++ js/main.js
@@ -1,10 +1,35 @@
-var head = document.getElementById('header');
+var head = document.getElementById("header");
+var flex2, flex3
 
 	window.onscroll = function() {
-		if(window.pageYOffset > 50){
-			header.classList.add('onscroll');
+		if (window.pageYOffset > 50) {
+			header.classList.add("onscroll");
 		} else {
-			header.classList.remove('onscroll');
+			header.classList.remove("onscroll");
 		}
 
+	}
+
+	// Remove second flex item for mobile view
+	function clearFlex() {
+		if (screen.width <= 680) {
+			flex3 = document.getElementsByClassName("flex-item1")[3];
+			flex3.style.display = "none";
+		} else {
+			flex3 = document.getElementsByClassName("flex-item1")[3];
+			flex3.style.display = "";
+		}
+	}
+
+	clearFlex();
+
+	// Menu toggle 
+
+	function openMenu() {
+		var x = document.getElementById("myTopNav");
+		if (x.className === "navtoggle") {
+			x.className = "mini";
+		} else {
+			x.className = "navtoggle";
+		}
 	}
